b808cb2c6e4c25238c0065498ee590badd78b23a
Aleksey Suprun <aleksey.suprun@gmail.com>
2020-11-27 11:10:54 +0200

    Deprected syntax fixed for apache-spark@2.4.0

--- Formula/apache-spark@2.4.0.rb
+++ Formula/apache-spark@2.4.0.rb
@@ -6,9 +6,13 @@ class ApacheSparkAT240 < Formula
   sha256 "c93c096c8d64062345b26b34c85127a6848cff95a4bb829333a06b83222a5cfa"
   head "https://github.com/apache/spark.git"
 
+  livecheck do
+    url :stable
+  end
+
   bottle :unneeded
 
-  depends_on :java => "1.8"
+  depends_on "openjdk@8"
 
   def install
     # Rename beeline to distinguish it from hive's beeline
@@ -17,10 +21,12 @@ class ApacheSparkAT240 < Formula
     rm_f Dir["bin/*.cmd"]
     libexec.install Dir["*"]
     bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk@8"].opt_prefix)
   end
 
   test do
-    assert_match "Long = 1000", pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1", "sc.parallelize(1 to 1000).count()")
+    assert_match "Long = 1000",
+      pipe_output(bin/"spark-shell --conf spark.driver.bindAddress=127.0.0.1",
+                  "sc.parallelize(1 to 1000).count()")
   end
 end
